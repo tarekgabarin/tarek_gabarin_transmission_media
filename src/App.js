24c0ceee0324002ad6faacf387b923b69ba386a9
tarekgabarin <tarekgabarin1992@gmail.com>
2019-04-12 19:23:27 -0400

    did the button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+        invoiceAmount: 500,
+        isEditable: false
+    }
+  }
+
   render() {
     return (
       <section>
@@ -11,11 +21,18 @@ class App extends Component {
             <div className="card">
               <div className="fields">
                 <div className="header-field">
-                    Pay Invoice
+                    <span>Pay Invoice</span>
                 </div>
                 <div className="main-fields">
-                  <div>1</div>
-                  <div>2</div>
+                  <span className={'payment-label'}>Payment Amount</span>
+                  <div className="invoice-amount">
+                    <div>
+                        <input type={'text'} className={'input'} value="" name='amount'/>
+                    </div>
+                    <div>
+                      <button className={'edit-button'} name={'edit-button'}>Edit</button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
