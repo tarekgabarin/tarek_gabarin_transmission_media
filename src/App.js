ad78e52af3b696a44dc32c0ac769e871f158f42e
tarekgabarin <tarekgabarin1992@gmail.com>
2019-04-12 16:23:07 -0400

    made the basic card css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section>
+
+        <div className="row">
+          <div className="column">
+            <div className="card">
+              <div className="fields">
+                <div className="header-field">
+                    Pay Invoice
+                </div>
+                <div className="main-fields">
+                  <div>1</div>
+                  <div>2</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </section>
     );
   }
 }
